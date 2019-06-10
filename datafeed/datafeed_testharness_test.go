d16d7650f6c5afd6ae9fe9d3ee969043800b9039
Mani <kodebot@hotmail.com>
2019-06-10 20:40:15 +0100

    update: testharness

--- datafeed/datafeed_testharness_test.go
+++ datafeed/datafeed_testharness_test.go
@@ -5,42 +5,67 @@ package datafeed
 import (
 	"io/ioutil"
 	"os"
+	"sort"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestHarnessDataFeedFromFeedInfo(t *testing.T) {
 
 	feedConfigPath := "./test_data/testharness/ready/"
-	files, _ := ioutil.ReadDir(feedConfigPath)
+	outputDir := "./test_data/testharness/output/"
+
+	err := os.RemoveAll(outputDir)
+	if err != nil {
+		t.Fatalf("unable to delete the output dir. error %s", err.Error())
+	}
+
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		err = os.MkdirAll(outputDir, os.ModePerm)
+		if err != nil {
+			t.Fatalf("unable to create output dir %s. error: %s", outputDir, err.Error())
+		}
+	} else {
+		t.Fatalf("unable to check if the output dir exist or not. error: %s", err.Error())
+	}
+
+	files, err := ioutil.ReadDir(feedConfigPath)
+	if err != nil {
+		t.Fatalf("unable to read datafeed info from %s. error: %s", feedConfigPath, err.Error())
+	}
 
 	for _, file := range files {
 		fullPath := feedConfigPath + file.Name()
 		dataFeed, _ := NewFromFeedInfo(fullPath)
 
-		outputPath := fullPath + ".txt"
+		outputPath := outputDir + file.Name() + ".txt"
 		dataFeedString := dataFeedToString(dataFeed)
-		ioutil.WriteFile(outputPath, []byte(dataFeedString), os.ModePerm)
-
-		// todo: delete old file
-		// sort the result by key
+		err = ioutil.WriteFile(outputPath, []byte(dataFeedString), os.ModePerm)
+		if err != nil {
+			t.Fatalf("unable to write to file %s. error: %s", outputPath, err.Error())
+		}
 	}
 }
 
 func dataFeedToString(feed []map[string]interface{}) string {
-	result := ""
+	result := []string{}
 	for _, record := range feed {
-		recordString := ""
+		fields := []string{}
 		for key, value := range record {
 			valueString := "NIL"
 			if value != nil {
-				valueString = value.(string)
+				if valueDate, ok := value.(*time.Time); ok {
+					valueString = valueDate.String()
+				} else {
+					valueString = value.(string)
+				}
 			}
-			recordString = recordString + "\n" + key + ":" + valueString
+			fields = append(fields, key+": "+valueString)
 		}
-		result = result + "\n***********************************************************************************************************\n"
-		result = result + "***********************************************************************************************************\n\n"
-		result = result + recordString
+		sort.Strings(fields)
+		result = append(result, strings.Join(fields, ",\n"))
 	}
 
-	return result
+	return strings.Join(result, "\n**********************************************************************************\n")
 }
