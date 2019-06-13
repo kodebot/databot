ac06b2a9e04276b357429c5da8c5c75339f6d5b5
Mani <kodebot@hotmail.com>
2019-06-13 22:06:09 +0100

    fix: avoid mad repeated scrapping

--- datafeed/record/collectors/rssatom/collector.go
+++ datafeed/record/collectors/rssatom/collector.go
@@ -1,7 +1,12 @@
 package rssatom
 
 import (
+	"github.com/golang/glog"
+	"github.com/kodebot/newsfeed/articles"
+	datapkg "github.com/kodebot/newsfeed/data"
 	field "github.com/kodebot/newsfeed/datafeed/record/collectors/field"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Collect returns collected fields from the given data using given field collector settings
@@ -9,6 +14,21 @@ func Collect(data string, fieldsInfo []field.Info) []map[string]interface{} {
 	feeds := readFromXML(data)
 	var records []map[string]interface{}
 	for _, item := range feeds {
+		//hack:
+
+		var existing articles.Article
+		articlesCollection, err := datapkg.GetCollection("articles")
+		err = datapkg.FindOne(articlesCollection, bson.M{"sourceurl": item.Link}, &existing)
+
+		if err != nil && err != mongo.ErrNoDocuments {
+			continue
+		}
+
+		if existing.SourceURL != "" {
+			glog.Infoln("item already found, not adding this to the store...")
+			continue
+		}
+
 		record := map[string]interface{}{}
 		for _, fieldInfo := range fieldsInfo {
 			record[fieldInfo.Name] = field.Create(item, fieldInfo)
