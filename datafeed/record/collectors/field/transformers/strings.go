22ce3d14b7482db61f3be084d89fce608d7a3e9e
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-06-20 08:48:09 +0100

    add: more oneindia news

--- datafeed/record/collectors/field/transformers/strings.go
+++ datafeed/record/collectors/field/transformers/strings.go
@@ -1,6 +1,7 @@
 package transformers
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/kodebot/newsfeed/logger"
@@ -39,11 +40,27 @@ func trimRight(value interface{}, parameters map[string]interface{}) interface{}
 func replace(value interface{}, parameters map[string]interface{}) interface{} {
 	if valueString, ok := value.(string); ok {
 
+		olds := []string{}
+
 		if old := parameters["old"]; old != nil {
+
+			if reflect.TypeOf(old).Kind() == reflect.Slice {
+				for _, oldItem := range old.([]interface{}) {
+					olds = append(olds, oldItem.(string))
+				}
+			} else {
+				olds = append(olds, old.(string))
+			}
+
 			if new := parameters["new"]; new != nil {
-				return strings.Replace(valueString, old.(string), new.(string), 1)
+				newstr := new.(string)
+				for _, oldstr := range olds {
+					valueString = strings.Replace(valueString, oldstr, newstr, 1)
+				}
 			}
 		}
+
+		return valueString
 	}
 
 	logger.Errorf("replace is not allowed on non string type")
