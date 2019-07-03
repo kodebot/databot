bce554b9e7b017614cf6889d04ecd66a0ef58ec4
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-07-03 15:55:19 +0100

    update: http reader tests

--- pkg/html/reader_test.go
+++ pkg/html/reader_test.go
@@ -10,13 +10,30 @@ import (
 )
 
 func TestReadAsString(t *testing.T) {
-	httpServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		panic(errors.New("foo"))
-	}))
-	defer func() { httpServer.Close() }()
 
-	reader := NewDocumentReader("bar")
-	actual, _ := reader.ReadAsString()
-	assert.Equal(t, "", actual)
+	tests := []struct {
+		httpError  bool
+		statusCode int
+		body       string
+		expected   string
+	}{
+		{true, 200, "bar", ""},
+		{false, 200, "bar", "bar"},
+		{false, 401, "bar", ""},
+	}
 
+	for _, test := range tests {
+		httpServer := httptest.NewServer(http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+			if test.httpError {
+				panic(errors.New("foo"))
+			}
+			res.WriteHeader(test.statusCode)
+			res.Write([]byte(test.body))
+		}))
+		defer func() { httpServer.Close() }()
+
+		reader := NewDocumentReader("http://bar")
+		actual, _ := reader.ReadAsString()
+		assert.Equal(t, test.expected, actual)
+	}
 }
