07a6f2304278c284289c7c151b1a6303b8510e37
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-07-08 09:41:41 +0100

    update: databot results are displayed

--- cmd/databot-testharness/main.go
+++ cmd/databot-testharness/main.go
@@ -1,18 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"errors"
+	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/kodebot/databot/pkg/config"
+	"github.com/kodebot/databot/pkg/databot"
+	"github.com/kodebot/databot/pkg/reccollector"
+	"github.com/kodebot/databot/pkg/rssatom"
+	"github.com/kodebot/databot/pkg/toml"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	res, _ := ioutil.ReadFile("./home.html")
-	fmt.Fprint(w, string(res))
+	feedSpecReader := toml.FeedSpecReader{}
+	feed := feedSpecReader.Read("feedconfig.toml")
+
+	var recCreator databot.RecordCreator
+	switch feed.RecordSpec.CollectorSpec.Type {
+	case reccollector.RssAtom:
+		recCreator = rssatom.NewRecordCreator()
+	case reccollector.HTMLSingle:
+		panic(errors.New("HTMLSingle record collector is not implemented"))
+	case reccollector.HTML:
+		panic(errors.New("HTMLMultiple record collector is not implemented"))
+	default:
+		panic(errors.New("Unsupported record collector"))
+	}
+
+	recs := recCreator.Create(feed.RecordSpec)
+	t, _ := template.ParseFiles("home.html")
+
+	t.Execute(w, recs)
 }
 
 func main() {
+	confBuilder := config.NewBuilder()
+	confBuilder.UseEnv()
+	confBuilder.Build()
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":9022", nil))
 }
