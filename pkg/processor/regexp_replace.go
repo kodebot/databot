8ba87f9723ad3db9b6ce50373f7c9a1e91780dab
Manikandan Venkatraman <mvenkatraman@jet2.com>
2019-08-09 13:36:00 +0100

    update: vikadan in progress

---
+++ pkg/processor/regexp_replace.go
@@ -0,0 +1,49 @@
+package processor
+
+import (
+	"strings"
+
+	"github.com/kodebot/databot/pkg/logger"
+	"github.com/kodebot/databot/pkg/stringutil"
+)
+
+func init() {
+	register("regexp:replace", regexpReplace)
+}
+
+func regexpReplace(params map[string]interface{}) Processor {
+	oldParam := params["old"]
+
+	if oldParam == nil {
+		logger.Fatalf("no old parameter found.")
+	}
+
+	old, ok := oldParam.(string)
+	if !ok {
+		logger.Fatalf("old must be string")
+	}
+
+	newParam := params["new"]
+
+	if newParam == nil {
+		logger.Fatalf("no new parameter found.")
+	}
+
+	new, ok := newParam.(string)
+	if !ok {
+		logger.Fatalf("new must be string")
+	}
+
+	return func(in <-chan interface{}, out chan<- interface{}) {
+		for newInput := range in {
+			block, ok := newInput.(string)
+			if !ok {
+				logger.Fatalf("unexpected input %#v. Input must be of type string", block)
+			}
+
+			match := stringutil.RegexpMatch(block, old)
+			block = strings.Replace(block, match, new, -1)
+			out <- block
+		}
+	}
+}
--- pkg/stringutil/regexp.go
+++ pkg/stringutil/regexp.go
@@ -88,3 +88,46 @@ func RegexpMatch(val string, expr string) string {
 	}
 	return ""
 }
+
+// RegexpReplaceAll returns all the matches from val using expr
+// when captured group name called 'data' is present, then only the 'data' sub-group is returned
+// returns empty []string when no match found
+func RegexpReplaceAll(val string, expr string, new string) string {
+	if val != "" {
+		if expr == "" {
+			logger.Errorf("no regular expression found")
+			return val
+		}
+
+		re, err := regexp.Compile(expr)
+		if err != nil {
+			logger.Errorf("invalid regexp: %s error: %s. \n", expr, err.Error())
+			return val
+		}
+
+		requiredMatchIndex := -1
+		for i, val := range re.SubexpNames() {
+			if val == "data" {
+				requiredMatchIndex = i
+			}
+		}
+
+		if requiredMatchIndex > -1 {
+			matches := re.FindAllStringSubmatch(val, -1)
+			if matches == nil || len(matches) < 1 {
+				logger.Warnf("no match found.")
+			}
+
+			for _, m := range matches {
+				if len(m) < requiredMatchIndex+1 {
+					logger.Warnf("no match found.")
+					return result
+				}
+				result = append(result, m[requiredMatchIndex])
+			}
+		} else { // when there is no captured group 'data' - just return the whole match
+			return re.ReplaceAllString(val, new)
+		}
+	}
+	return result
+}
